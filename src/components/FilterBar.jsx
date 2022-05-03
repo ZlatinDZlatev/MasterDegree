e63509979f74dbd4b1946c2dc0cc73adae1d2a02
ZlatinDZlatev <zlatin98@abv.bg>
2022-05-03 13:26:55 +0300

    Example

---
+++ src/components/FilterBar.jsx
@@ -0,0 +1,51 @@
+import {  FormControl, FormControlLabel, FormGroup, Typography } from '@material-ui/core'
+import React from 'react'
+import { LaptopFilter } from './LaptopsFilter.js'
+import { makeStyles } from '@material-ui/core/styles';
+import GreenCheckbox from '../basic/GreenCheckbox.jsx';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  checkBoxes:{
+      display:"flex",
+      flexDirection:"column"
+  }
+       
+}));
+
+const FilterBar = ({checkBoxesBrand, setCheckBoxesBrand}) =>{
+    const classes=useStyles()      
+    const handleCheckBoxBrand = (event) => {
+        setCheckBoxesBrand({ ...checkBoxesBrand, [event.target.name]: event.target.checked });
+      };
+      return(
+          <div className={classes.checkBoxes}>
+                <FormControl>
+                <Typography component="legend">Brands</Typography>
+                    <FormGroup>
+
+                        {  LaptopFilter.map((item, index)=>{
+                            return(
+                                <div className={classes.checkBoxes}>
+                      {item.filterType==="brand"?item.filterOptions.map((item, index)=>{
+                            return(
+                                <FormControlLabel
+                    control={<GreenCheckbox defaultChecked={true} color="success"  
+                    onChange={handleCheckBoxBrand} name={item.name}/>}
+                    label={item.label}
+                  />
+                            )
+                      }):null}     
+                                </div>              
+                            )
+                        })}
+                    </FormGroup>
+                </FormControl>
+                </div>
+
+      )
+}
+export default FilterBar
+
+             
